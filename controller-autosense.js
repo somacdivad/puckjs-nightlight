5c1752a857f7cd41c36f76860e658a6fd61e885b
David Amos <somacdivad@gmail.com>
2016-12-28 21:57:32 -0600

    Updated auto sense

--- controller-autosense.js
+++ controller-autosense.js
@@ -8,8 +8,9 @@
  * Control the color changing night light via a second Puck.js and UART
  */
 
-const _NAME = 'Puck.js 9fb2',  // name of the puck to be controlled
-      _TRIGGER = 0.3;
+const _NAME = 'Puck.js 9fb2';  // name of the puck to be controlled
+const _SENSE_INTERVAL = 1000;  // time between light sensor readings
+const _SENSE_TRIGGER = 0.1;    // light trigger intensity for toggling night light on/off
 
 var uart = require('ble_simple_uart'),
     ambTimeout = false,
@@ -18,32 +19,33 @@ var uart = require('ble_simple_uart'),
 // function for sending toggleLights command to remote puck via UART
 function sendToggle() {
   try {
-    NRF.requestDevice({ timeout: 5000, filters: [{ name: _NAME }] }).then((dev) => {
-      uart.write(dev, 'toggleLights()\n');
+    NRF.requestDevice({ filters: [{ name: _NAME }] }).then((dev) => {
+      uart.write(dev, 'toggleLights()\r');
     }).then(() => {
       on = !on;
-      ambTimeout = ambientSense();
+      ambientSense();
     }).catch((e) => {
       console.log('error', e);
-      ambTimeout = ambientSense();
+      ambientSense();
     });
   } catch (e) {
     console.log('error', e);
-    ambTimeout = ambientSense();
+    ambientSense();
   }
 }
 
+// function for starting ambient light sensor
+// returns a reference to an interval set by setInterval
 function ambientSense() {
-  var timeout = setInterval(() => {
+  ambTimeout = setInterval(() => {
     var l = Puck.light(),
-        turnOn = (l <= _TRIGGER) && !on,
-        turnOff = (l > _TRIGGER) && on;
+        turnOn = (l <= _SENSE_TRIGGER) && !on,
+        turnOff = (l > _SENSE_TRIGGER) && on;
     if (turnOn || turnOff) {
       if (ambTimeout) clearInterval(ambTimeout);
       sendToggle();
     }
-  });
-   return timeout;
+  }, _SENSE_INTERVAL);
 }
 
-ambTimeout = ambientSense();
+ambientSense();
